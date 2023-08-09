d515c1723c5a6a5818d85cefa1ba426844ba7f9d
jjo3ys <jjo3ys@gmail.com>
2023-08-09 17:08:16 +0900

    first commit on backend

---
+++ src/main/java/com/day24/preProject/PreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.day24.preProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/day24/preProject/PreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.day24.preProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
