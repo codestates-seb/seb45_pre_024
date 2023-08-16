a70121cf58708fad66bda900ce2b5ca0106d44a4
jjo3ys <jjo3ys@gmail.com>
2023-08-16 14:10:15 +0900

    question comment function temporary

--- src/main/java/com/day24/preProject/questionComment/repository/QustionCommentRepository.java
+++ src/main/java/com/day24/preProject/questionComment/repository/QustionCommentRepository.java
@@ -1,17 +1,15 @@
 package com.day24.preProject.questionComment.repository;
 
-import com.codestates.QustionComment.entity.QustionComment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface QustionCommentRepository extends JpaRepository<QustionComment, Long> {
-    Optional<QustionComment> findByQustionCommentCode(String QustionCommentCode);
-
-    //    @Query(value = "FROM QustionComment c WHERE c.QustionCommentId = :QustionCommentId")
-//    @Query(value = "SELECT * FROM QustionComment WHERE QustionComment_Id = :QustionCommentId", nativeQuery =true)
-    @Query(value = "SELECT c FROM QustionComment c WHERE c.QustionCommentId = :QustionCommentId")
-    Optional<QustionComment> findByQustionComment(long QustionCommentId);
+public interface QustionCommentRepository extends JpaRepository {
+//    Optional<QustionComment> findByQustionCommentCode(String QustionCommentCode);
+//
+//    //    @Query(value = "FROM QustionComment c WHERE c.QustionCommentId = :QustionCommentId")
+////    @Query(value = "SELECT * FROM QustionComment WHERE QustionComment_Id = :QustionCommentId", nativeQuery =true)
+//    @Query(value = "SELECT c FROM QustionComment c WHERE c.QustionCommentId = :QustionCommentId")
+//    Optional<QustionComment> findByQustionComment(long QustionCommentId);
 }
 
