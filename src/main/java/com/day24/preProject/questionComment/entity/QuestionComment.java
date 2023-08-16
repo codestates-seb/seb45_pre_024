12dd9bd69b9af55ea39654089012049ad97da4d9
jjo3ys <jjo3ys@gmail.com>
2023-08-16 14:47:19 +0900

    question comment structure

--- src/main/java/com/day24/preProject/questionComment/entity/QuestionComment.java
+++ src/main/java/com/day24/preProject/questionComment/entity/QuestionComment.java
@@ -1,2 +1,20 @@
-package com.day24.preProject.questionComment.entity;public class QuestionComment {
+package com.day24.preProject.questionComment.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class QuestionComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long question_comment_id;
 }
