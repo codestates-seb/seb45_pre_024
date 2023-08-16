9d20efe23669072b8a4284794b9a7fa55798a466
SonSeolHui <alck1012@gmail.com>
2023-08-16 11:25:56 +0900

    signin class명 수정

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -9,14 +9,15 @@ function Footer() {
             <img className="logo" src={envURL + '/logo.png'} alt="logo"></img>
           </div>
           <div className="stack_overflow">
-            <h5>STACK OVERFLOW</h5>
-            <li>Questions</li>
-            <li>Help</li>
+            <h5 className="h5">STACK OVERFLOW</h5>
+            <li className="li">Questions</li>
+            <li className="li">Help</li>
           </div>
           <div className="team_notion">
-            <h5>TEAM</h5>
-            <li>
+            <h5 className="h5">TEAM</h5>
+            <li className="li">
               <a
+                className="a"
                 href="https://www.notion.so/codestates/bed61b7145874133b9fa28a29a7156aa?pvs=4"
                 target="_blank"
                 rel="noreferrer"
@@ -26,10 +27,11 @@ function Footer() {
             </li>
           </div>
           <div className="member_fe">
-            <h5>MEMBER</h5>
-            <li>Frontend Developer</li>
-            <li>
+            <h5 className="h5">MEMBER</h5>
+            <li className="li">Frontend Developer</li>
+            <li className="li">
               <a
+                className="a"
                 href="https://github.com/ysm990926"
                 target="_blank"
                 rel="noreferrer"
@@ -37,8 +39,9 @@ function Footer() {
                 윤선문
               </a>
             </li>
-            <li>
+            <li className="li">
               <a
+                className="a"
                 href="https://github.com/yejicho-helloworld"
                 target="_blank"
                 rel="noreferrer"
@@ -46,8 +49,9 @@ function Footer() {
                 조예지
               </a>
             </li>
-            <li>
+            <li className="li">
               <a
+                className="a"
                 href="https://github.com/SonSeolHui"
                 target="_blank"
                 rel="noreferrer"
@@ -57,10 +61,11 @@ function Footer() {
             </li>
           </div>
           <div className="member_be">
-            <h5>&nbsp;</h5>
-            <li>Backend Developer</li>
-            <li>
+            <h5 className="h5">&nbsp;</h5>
+            <li className="li">Backend Developer</li>
+            <li className="li">
               <a
+                className="a"
                 href="https://github.com/jjo3ys"
                 target="_blank"
                 rel="noreferrer"
@@ -68,8 +73,9 @@ function Footer() {
                 조연성
               </a>
             </li>
-            <li>
+            <li className="li">
               <a
+                className="a"
                 href="https://github.com/kangsuck"
                 target="_blank"
                 rel="noreferrer"
@@ -77,8 +83,9 @@ function Footer() {
                 이강석
               </a>
             </li>
-            <li>
+            <li className="li">
               <a
+                className="a"
                 href="https://github.com/minprr1029"
                 target="_blank"
                 rel="noreferrer"
@@ -88,7 +95,7 @@ function Footer() {
             </li>
           </div>
           <div className="info">
-            <p>
+            <p className="p">
               Site design / logo © 2023 Stack Exchange Inc; user
               <br />
               contributions licensed under CC BY-SA.
