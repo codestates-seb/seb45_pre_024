ab2e83f5c0d2014a412625c6eebde1dae37a9797
jjo3ys <jjo3ys@gmail.com>
2023-08-22 17:29:15 +0900

    fix server timezone utc to asio/seoul

--- src/main/java/com/day24/preProject/PreProjectApplication.java
+++ src/main/java/com/day24/preProject/PreProjectApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class PreProjectApplication {
@@ -11,5 +14,8 @@ public class PreProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PreProjectApplication.class, args);
 	}
-
+	@PostConstruct
+	public void init(){
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 }
